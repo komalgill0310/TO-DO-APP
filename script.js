c21d675f257b7455b702183cb81919aa252761f6
Komal Gill <komalgill0310@gmail.com>
2021-08-18 11:47:04 -0400

    basic code

---
+++ script.js
@@ -0,0 +1,11 @@
+// Don't use document.write. If you want to add content to a page you need to work with the DOM. Google "create div javascript" or "create span javascript" for examples, you basically need to create an element that has your text in it and add that element to the part of the page you want the text to display.
+
+
+
+
+
+  
+
+  
+
+
