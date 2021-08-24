d30d9a6b59dcd80a495f1aeb8ff205d68264421a
Komal Gill <komalgill0310@gmail.com>
2021-08-24 15:11:08 -0400

    radio funtion created

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
+  const li = document.getElementById('list');;
   const submit = document.getElementById('submit');
+
+  submit.addEventListener('click',radioButton);  
   submit.addEventListener('click',display);
+
+  function radioButton(){
+    const radio = document.createElement('input');
+    radio.setAttribute('type','radio');
+    li.appendChild(radio);
+    console.log(radio);
+    // if(radio.onclick==true){
+    //   result.strike();
+    }  
 
 function display(){
   console.log('this function ran');
   const input = document.getElementById('task');
   const toDo = input.value;
-  var t = document.createTextNode(toDo);
-  console.log(t);
-  const li = document.getElementById('list');
-  const result = li.appendChild(t);
-  // console.log(result);
+  const textNode = document.createTextNode(toDo);
+  console.log(textNode);
+  // const li = document.getElementById('list');
+  li.appendChild(textNode);
+  li.appendChild(document.createElement('br'));
 }
+
+// function checkStrike(){
+//   console.log("ran");
+//   if(radio.checked==true){
+//     strike();}
+// }
 
 
 
